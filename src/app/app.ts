ba97cd37535136f9eef59bbabb7bd8c47f038c5b
YAMI Marcel <yamitmarcel@gmail.com>
2026-06-22 10:39:43 +0100

    section témoignages avec étoiles dynamiques

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { Menu } from './components/menu/menu';
+import { Temoignages } from './components/temoignages/temoignages';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, Menu],
+  imports: [RouterOutlet, Header, Hero, Menu, Temoignages], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
