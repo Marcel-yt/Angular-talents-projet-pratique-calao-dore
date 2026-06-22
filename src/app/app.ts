862b72658b7ea2caefe12546fd304f2caaea9f2f
YAMI Marcel <yamitmarcel@gmail.com>
2026-06-22 10:46:31 +0100

    section horaires avec statut jour actuel

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { Menu } from './components/menu/menu';
 import { Temoignages } from './components/temoignages/temoignages';
+import { Horaires } from './components/horaires/horaires';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, Menu, Temoignages], 
+  imports: [RouterOutlet, Header, Hero, Menu, Temoignages, Horaires], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
